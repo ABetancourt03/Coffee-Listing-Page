0e9220780f137868e0fd4dbfdfeb26886eb01089
ABetancourt03 <neutralzgamer@gmail.com>
2024-03-30 20:08:32 -0400

    Add Linter and Formatter

--- src/services/coffees.ts
+++ src/services/coffees.ts
@@ -1,18 +1,21 @@
 export const getAllCoffees = async () => {
-  const res = await fetch("https://raw.githubusercontent.com/devchallenges-io/web-project-ideas/main/front-end-projects/data/simple-coffee-listing-data.json", {
-    method: "GET",
-    headers: {
-      "Content-type": "application/json",
-    }
-  });
+	const res = await fetch(
+		"https://raw.githubusercontent.com/devchallenges-io/web-project-ideas/main/front-end-projects/data/simple-coffee-listing-data.json",
+		{
+			method: "GET",
+			headers: {
+				"Content-type": "application/json",
+			},
+		}
+	)
 
-  const coffees = (await res.json());
-  return coffees;
+	const coffees = await res.json()
+	return coffees
 }
 
 export const getAvailableCoffees = async () => {
-  const coffees = await getAllCoffees()
-  let filteredCoffes = coffees.filter(coffee => coffee.available === true)
+	const coffees = await getAllCoffees()
+	const filteredCoffes = coffees.filter((coffee) => coffee.available === true)
 
-  return filteredCoffes
+	return filteredCoffes
 }
